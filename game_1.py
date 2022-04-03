61f571b811a6962c7057f46834cefbcf0783c91e
AdamBarnas <101982972+AdamBarnas@users.noreply.github.com>
2022-04-03 23:59:50 +0200

    kolorowe koła

--- game_1.py
+++ game_1.py
@@ -1,74 +1,61 @@
 import sys
 import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((800, 640))
+from player import Player
 
 print(pygame.__version__)
 
-kwadrat = pygame.Rect(10, 20, 100, 100)
-R = G = B = 255
-n = 0
-clock = pygame.time.Clock()
-dt = 0.0
-rain = 0.0
+class Game(object):
+    def __init__(self):
+        #Conf
+        self.res = (800, 640)
+        self.tickrate = 1/120.0
+        # 1/max_fps
 
-tick = 1/40.0
-# 1/max_fps
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.res)
 
-while True:
-    # eventy
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit(0)
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_x:
-            kwadrat.y += 10
-    # input
-    inkey = pygame.key.get_pressed()
+        self.clock = pygame.time.Clock()
+        self.cl_dt = 0.0
 
-    # tickrate
-    dt += clock.tick()/1000.0
-    while dt > tick:
-        dt -= tick
-        # move
+        self.kwadrat = pygame.Rect(30, 30, 42, 42)
+        self.R = self.G = self.B = 255
+        self.n = 0
 
-        speed = 5
+        self.player = Player(self)
 
-        if inkey[pygame.K_a]:
-            kwadrat.x -= speed
-        if inkey[pygame.K_s]:
-            kwadrat.y += speed
-        if inkey[pygame.K_d]:
-            kwadrat.x += speed
-        if inkey[pygame.K_w]:
-            kwadrat.y -= speed
-
-        # borders
-        if kwadrat.x <= -50:
-            kwadrat.x = 750
-        if kwadrat.y <= 0:
-            kwadrat.y = 640
-        if kwadrat.x >= 801:
-            kwadrat.x = 0
-        if kwadrat.y >= 641:
-            kwadrat.y = 0
+        while True:
+            # eventy
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                    self.player.ruch("d")
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+                    self.player.ruch("a")
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+                    self.player.ruch("w")
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                    self.player.ruch("s")
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    self.player.ruch("space")
 
 
-        # rainbow
-        if n < 250:
-            R -= 4
-            G -= 4
-            B -= 4
-        elif n < 500:
-            R += 4
-            G += 4
-            B += 4
-        else:
-            n = 0
-        n += 4
+            # tick
+            self.cl_dt += self.clock.tick()/1000
+            while self.cl_dt > self.tickrate:
+                self.tick()
+                self.cl_dt -= self.tickrate
 
-    # draw
-    kolor = (R, G, B)
-    pygame.draw.rect(screen, kolor, kwadrat)
-    pygame.display.flip()
-    screen.fill((0, 0, 0))
+            # draw
+            self.screen.fill((0, 0, 0))
+            self.draw()
+            pygame.display.flip()
+
+    def tick(self):
+        self.player.tick()
+
+    def draw(self):
+        self.player.draw()
+
+if __name__ == "__main__":
+    Game()
