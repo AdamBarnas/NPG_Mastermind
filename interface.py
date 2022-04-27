37a003f892129742455aa4aee7451f007163624c
AdamBarnas <101982972+AdamBarnas@users.noreply.github.com>
2022-04-27 08:05:45 +0200

    poziom_trudnosci

--- interface.py
+++ interface.py
@@ -5,15 +5,7 @@ class Interface(object):
 
     def __init__(self, game):
         self.game = game
-        # Colours
-        self.yellow = (255, 255, 0)
-        self.red = (255, 0, 0)
-        self.green = (0, 255, 0)
-        self.blue = (0, 0, 255)
-        self.orange = (255, 128, 0)
-        self.purple = (200, 0, 255)
-        self.white = (255, 255, 255)
-        self.gray = (128, 128, 128)
 
     def draw(self):
         pygame.draw.rect(self.game.screen, (50, 50, 50), pygame.Rect(0, 0, 800, 640))
+
